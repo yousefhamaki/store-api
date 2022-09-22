6346ca7922080e147433c4133d8b5f1131a3b02a
yousef hamaki <75567669+yousefhamaki@users.noreply.github.com>
2022-09-22 06:47:23 +0200

    Delete build directory

--- build/app/middleware/Error.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorMiddleware = (err, _req, res, _next) => {
-    const status = err.status || 500;
-    const message = err.message || "oops! some thing went wrong";
-    res.status(status).json({ status, message });
-};
-exports.default = errorMiddleware;
--- build/app/middleware/unauth.error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const HandleUnauthError = (next) => {
-    const err = new Error("error Auth: please try again");
-    err.status = 401;
-    next(err);
-};
-exports.default = HandleUnauthError;
